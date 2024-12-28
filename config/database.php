70b590b79835c1844a610e9e26e6a187e0a5f2a7
aistiak <aristiak1994@gmail.com>
2024-12-28 11:55:11 +0600

    add redis

--- config/database.php
+++ config/database.php
@@ -121,7 +121,7 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
+            'password' => env('REDIS_PASSWORD', 'secret'),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
         ],
